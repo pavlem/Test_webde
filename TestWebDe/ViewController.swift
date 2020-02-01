5a2f2ba3c76cf92ad629b7b0116aec08b61d227e
Pavle Mijatovic <paul.mijatovic@sprintingsoftware.com>
2020-02-01 11:52:55 +0100

    1

---
+++ TestWebDe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestWebDe
+//
+//  Created by Pavle Mijatovic on 01/02/2020.
+//  Copyright © 2020 Pavle Mijatovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
