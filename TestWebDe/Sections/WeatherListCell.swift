6cf0ce69056f9718856cd2358b76af0a21a3b70f
Pavle Mijatovic <paul.mijatovic@sprintingsoftware.com>
2020-02-05 08:32:35 +0100

    15

--- TestWebDe/Sections/WeatherListCell.swift
+++ TestWebDe/Sections/WeatherListCell.swift
@@ -10,15 +10,33 @@ import UIKit
 
 class WeatherListCell: UITableViewCell {
     
+    // MARK: - API
+    var weatherListVM: WeatherListVM? {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    // MARK: Properties
     @IBOutlet weak var dayLbl: UILabel!
     @IBOutlet weak var avgTempLbl: UILabel!
     @IBOutlet weak var startTimeLbl: UILabel!
     @IBOutlet weak var weatherIconImgView: UIImageView!
     
-    var weatherListVM: WeatherListVM? {
-        didSet {
-            updateUI()
-        }
+
+
+    // MARK: Lifecycle
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setUI()
+    }
+    
+    // MARK: Private
+    private func setUI() {
+        dayLbl.font = UIFont.boldSystemFont(ofSize: 16)
+        avgTempLbl.font = UIFont.systemFont(ofSize: 16)
+        startTimeLbl.font = UIFont.systemFont(ofSize: 16)
     }
     
     private func updateUI() {
@@ -26,17 +44,52 @@ class WeatherListCell: UITableViewCell {
         avgTempLbl.text = weatherListVM?.averageTemp
         startTimeLbl.text = weatherListVM?.startingTime
         
+        if let iconString = weatherListVM?.iconString {
+            setIcon(iconCode: iconString)
+        }
+    }
+    
+    private func setIcon(iconCode: String) {
+        
+        guard let url = URL(string: "https://openweathermap.org/img/wn/\(iconCode)@2x.png") else { return }
+        getData(from: url) { (data, response, error) in
+            
+            guard
+            let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+            let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+            let data = data, error == nil,
+            let image = UIImage(data: data)
+            else { return }
+            
+            DispatchQueue.main.async() {
+                self.weatherIconImgView.image = image
+            }
+        }
     }
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    private func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
+
+//extension UIImageView {
+//    private func downloaded(from url: URL, contentMode mode:  UIView.ContentMode = .scaleToFill) {
+//        contentMode = mode
+//        URLSession.shared.dataTask(with: url) { data, response, error in
+//            guard
+//                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+//                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+//                let data = data, error == nil,
+//                let image = UIImage(data: data)
+//                else { return }
+//            DispatchQueue.main.async() {
+//                self.image = image
+//            }
+//        }.resume()
+//    }
+//
+//    func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {
+//        guard let url = URL(string: link) else { return }
+//        downloaded(from: url, contentMode: mode)
+//    }
+//}
