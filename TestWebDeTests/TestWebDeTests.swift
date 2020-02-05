f7879476b2bb3b8f4d2768bf1e3b4dfff23a8c9e
Pavle Mijatovic <paul.mijatovic@sprintingsoftware.com>
2020-02-05 17:06:19 +0100

    23

--- TestWebDeTests/TestWebDeTests.swift
+++ TestWebDeTests/TestWebDeTests.swift
@@ -19,16 +19,31 @@ class TestWebDeTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func testWeatherVM() {
+        
+        WeatherFiveDaysService().getMocFiveDayData { (weatherFiveDaysResponse) in
+            let days = WeatherPerDay.handle(wList: weatherFiveDaysResponse.list)
+            let day1 = days.first!
+            let day2 = days[4]
+            let day3 = days[5]
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+            let weatherListVM1 = WeatherListVM(weatherPerDay: day1)
+            let weatherListVM2 = WeatherListVM(weatherPerDay: day2)
+            let weatherListVM3 = WeatherListVM(weatherPerDay: day3)
+
+            print("ssssss")
+            
+            XCTAssert(weatherListVM1.averageTemp == "Average temp: 283 K", "testWeatherVM not ok")
+            XCTAssert(weatherListVM1.day == "Date: 2017-01-30", "testWeatherVM not ok")
+            XCTAssert(weatherListVM1.startingTime == "Starting time: 18:00", "testWeatherVM not ok")
+
+            XCTAssert(weatherListVM2.averageTemp == "Average temp: 285 K", "testWeatherVM not ok")
+            XCTAssert(weatherListVM2.day == "Date: 2017-02-03", "testWeatherVM not ok")
+            XCTAssert(weatherListVM2.startingTime == "Starting time: 00:00", "testWeatherVM not ok")
+            
+            XCTAssert(weatherListVM3.averageTemp == "Average temp: 284 K", "testWeatherVM not ok")
+            XCTAssert(weatherListVM3.day == "Date: 2017-02-04", "testWeatherVM not ok")
+            XCTAssert(weatherListVM3.startingTime == "Starting time: 00:00", "testWeatherVM not ok")
         }
     }
-
 }
